ee64138ec6f63126af47f452e3099303184951fa
geeta <geetanjali.pusuluri@cgifederal.com>
2018-07-17 08:51:58 -0400

    js file for july branch

---
+++ GitDemoWebApp/WebApplication1/Ajaxjs.js
@@ -0,0 +1,18 @@
+﻿function loadDoc(url, cFunction) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function myFunction1(xhttp) {
+    // action goes here
+}
+function myFunction2(xhttp) {
+    // action goes here
+}
