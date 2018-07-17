eec5c9a9c528b1ea561acb18a76d2ef32344ff11
geeta <geetanjali.pusuluri@cgifederal.com>
2018-07-16 22:07:32 -0400

    js,html,aspx files

---
+++ GitDemoWebApp/WebApplication1/JSExpressions.js
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Expressions</h2>
+
+<p>Expressions compute to values.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "John" + " "  + "Doe";
+</script>
+
+</body>
+</html>
---
+++ GitDemoWebApp/WebApplication1/SetHours.js
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to display a date after changing the hours.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var d = new Date();
+    d.setHours(15);
+    document.getElementById("demo").innerHTML = d;
+}
+</script>
+
+</body>
+</html>
